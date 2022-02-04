3524ab374b02efa76e187613e96826e70fac8d27
Prachi Khobragade <pkhobragade@linkedin.com>
2022-02-03 16:46:03 -0800

    Junit changed from version 4 to 5, changed tests accordingly
    Corrected  the incosistent behaviour in tests when it was checking the runtime free memory. Cannot rely on runTime memory for tests.

--- clients/src/test/java/org/apache/kafka/common/memory/WeakMemoryPoolTest.java
+++ clients/src/test/java/org/apache/kafka/common/memory/WeakMemoryPoolTest.java
@@ -18,17 +18,21 @@
 package org.apache.kafka.common.memory;
 
 import java.nio.ByteBuffer;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class WeakMemoryPoolTest {
     private static final int FORTY_MEGABYTES = 40 * 1024 * 1024;
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testNegativeAllocation() {
         WeakMemoryPool memoryPool = new WeakMemoryPool();
-        memoryPool.tryAllocate(-1);
+        assertThrows(IllegalArgumentException.class, () -> memoryPool.tryAllocate(-1));
     }
 
     @Test
@@ -37,34 +41,31 @@ public class WeakMemoryPoolTest {
         memoryPool.tryAllocate(0);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
     public void testNullRelease() {
         WeakMemoryPool memoryPool = new WeakMemoryPool();
-        memoryPool.release(null);
+        assertThrows(IllegalArgumentException.class, () -> memoryPool.release(null));
     }
 
     @Test
     public void testAllocationMemorySize() {
         WeakMemoryPool pool = new WeakMemoryPool();
-        long freeMemory = Runtime.getRuntime().freeMemory();
         ByteBuffer buffer1 = pool.tryAllocate(FORTY_MEGABYTES + 1);
         ByteBuffer buffer2 = pool.tryAllocate(FORTY_MEGABYTES + 2);
         ByteBuffer buffer3 = pool.tryAllocate(FORTY_MEGABYTES + 3);
-        Assert.assertTrue(Runtime.getRuntime().freeMemory()
-            <= freeMemory - buffer1.capacity() - buffer2.capacity() - buffer3.capacity());
 
         pool.release(buffer1);
         ByteBuffer reuse1 = pool.tryAllocate(FORTY_MEGABYTES);
         // Compare the references
-        Assert.assertTrue(reuse1 == buffer1);
+        assertTrue(reuse1 == buffer1);
 
         pool.release(buffer2);
         pool.release(buffer3);
         ByteBuffer reuse3 = pool.tryAllocate(FORTY_MEGABYTES + 3);
         ByteBuffer reuse2 = pool.tryAllocate(FORTY_MEGABYTES + 2);
 
-        Assert.assertTrue(reuse3 == buffer3);
-        Assert.assertTrue(reuse2 == buffer2);
+        assertTrue(reuse3 == buffer3);
+        assertTrue(reuse2 == buffer2);
     }
 
     @Test
@@ -77,15 +78,15 @@ public class WeakMemoryPoolTest {
         pool.release(buffer1);
         ByteBuffer reuse1 = pool.tryAllocate(FORTY_MEGABYTES);
         // Compare the references
-        Assert.assertEquals(System.identityHashCode(reuse1), System.identityHashCode(buffer1));
+        assertEquals(System.identityHashCode(reuse1), System.identityHashCode(buffer1));
 
         pool.release(buffer2);
         pool.release(buffer3);
         ByteBuffer reuse3 = pool.tryAllocate(FORTY_MEGABYTES + 3);
         ByteBuffer reuse2 = pool.tryAllocate(FORTY_MEGABYTES + 2);
 
-        Assert.assertEquals(System.identityHashCode(reuse3), System.identityHashCode(buffer3));
-        Assert.assertEquals(System.identityHashCode(reuse2), System.identityHashCode(buffer2));
+        assertEquals(System.identityHashCode(reuse3), System.identityHashCode(buffer3));
+        assertEquals(System.identityHashCode(reuse2), System.identityHashCode(buffer2));
     }
 
     @Test
@@ -108,9 +109,9 @@ public class WeakMemoryPoolTest {
         pool.release(buffer2);
         pool.release(buffer3);
 
-        Assert.assertEquals(identifier2, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 3)));
-        Assert.assertEquals(identifier3, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 7)));
-        Assert.assertEquals(identifier1, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 0)));
+        assertEquals(identifier2, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 3)));
+        assertEquals(identifier3, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 7)));
+        assertEquals(identifier1, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 0)));
 
         pool.release(buffer1);
         pool.release(buffer2);
@@ -124,8 +125,8 @@ public class WeakMemoryPoolTest {
         System.gc();
 
         // Assert that the object is a different one!
-        Assert.assertNotEquals(identifier2, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 3)));
-        Assert.assertNotEquals(identifier3, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 7)));
-        Assert.assertNotEquals(identifier1, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 0)));
+        assertNotEquals(identifier2, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 3)));
+        assertNotEquals(identifier3, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 7)));
+        assertNotEquals(identifier1, System.identityHashCode(pool.tryAllocate(FORTY_MEGABYTES + 0)));
     }
 }
