841074ecbac2a2981bffd7daafbd17e306a18c1a
Ke Hu <kehu@kehu-mn2.linkedin.biz>
2022-02-10 15:24:05 -0800

    [LI-HOTFIX] Resolve the bootstrap server when cluster metadata hasn't been refreshed for a long time

--- clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
+++ clients/src/main/java/org/apache/kafka/clients/consumer/KafkaConsumer.java
@@ -710,7 +710,8 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
             }
             ClusterResourceListeners clusterResourceListeners = configureClusterResourceListeners(keyDeserializer, valueDeserializer, reporters, interceptorList);
             this.metadata = new Metadata(retryBackoffMs, config.getLong(ConsumerConfig.METADATA_MAX_AGE_CONFIG),
-                config.getBoolean(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG), false, clusterResourceListeners);
+                config.getBoolean(ConsumerConfig.ALLOW_AUTO_CREATE_TOPICS_CONFIG), false, clusterResourceListeners,
+                Metadata.TOPIC_EXPIRY_MS, config.getLong(ConsumerConfig.LI_CLIENT_CLUSTER_METADATA_EXPIRE_TIME_MS_CONFIG));
             List<InetSocketAddress> addresses = ClientUtils.parseAndValidateAddresses(config.getList(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG));
             this.metadata.update(Cluster.bootstrap(addresses), Collections.<String>emptySet(), 0);
             String metricGrpPrefix = "consumer";
@@ -743,7 +744,8 @@ public class KafkaConsumer<K, V> implements Consumer<K, V> {
                     true,
                     new ApiVersions(),
                     throttleTimeSensor,
-                    logContext);
+                    logContext,
+                    config.getList(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG));
             netClient.setEnableStickyMetadataFetch(config.getBoolean(CommonClientConfigs.ENABLE_STICKY_METADATA_FETCH_CONFIG));
             netClient.setEnableClientResponseWithFinalize(config.getBoolean(ConsumerConfig.ENABLE_CLIENT_RESPONSE_LEAK_CHECK));
             this.client = new ConsumerNetworkClient(logContext, netClient, metadata, time, retryBackoffMs,
